5cf24522bd07432ce37d146017c5e31bcab01fea
HypeMan21 <bogdan07052005@gmail.com>
2024-02-29 14:38:29 +0200

    init

--- CharpLabs2024/Program.cs
+++ CharpLabs2024/Program.cs
@@ -7,7 +7,6 @@ class Program
         Console.WriteLine("Введіть три цілі числа:");
         int[] numbers = new int[3];
 
-        // Зчитування трьох цілих чисел
         for (int i = 0; i < 3; i++)
         {
             Console.Write($"Число {i + 1}: ");
@@ -16,7 +15,6 @@ class Program
 
         Console.WriteLine("Числа, які належать інтервалу [1, 18]:");
 
-        // Вибір чисел, які належать інтервалу [1, 18]
         foreach (int num in numbers)
         {
             if (num >= 1 && num <= 18)
