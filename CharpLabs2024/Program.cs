36eac03a3f1eea8afbd6e48cc51171f5be4ebdca
HypeMan21 <bogdan07052005@gmail.com>
2024-02-29 14:13:13 +0200

    init

--- CharpLabs2024/Program.cs
+++ CharpLabs2024/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 
-namespace CharpLabs2024
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Введіть три цілі числа:");
+        int[] numbers = new int[3];
+
+        // Зчитування трьох цілих чисел
+        for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine("hello123!");
-            Console.WriteLine("lili");
+            Console.Write($"Число {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Числа, які належать інтервалу [1, 18]:");
+
+        // Вибір чисел, які належать інтервалу [1, 18]
+        foreach (int num in numbers)
+        {
+            if (num >= 1 && num <= 18)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
