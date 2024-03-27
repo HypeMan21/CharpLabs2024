135467676edf4bf025a677c30fb09df0ded755a4
HypeMan21 <bogdan07052005@gmail.com>
2024-03-27 12:42:03 +0200

    init

--- CharpLabs2024/Program.cs
+++ CharpLabs2024/Program.cs
@@ -6,7 +6,7 @@ namespace CharpLabs2024
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Kolya!");
         }
     }
 }
